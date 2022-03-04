91e5a0667a01809164b5a382b322d3af1ab461db
paoka <paoka@LAPTOP-1016MKVC>
2022-03-04 17:01:50 +0200

    upload exerise 4

---
+++ Session-04/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_04
+{
+    internal class Class1
+    {
+
+        public Class1(){
+        } 
+        
+
+
+
+
+
+
+        public string ReverseString(string s)
+        {
+            string str, str1 =string.Empty;
+            int i, l;
+            str = "achilleas blekos";
+
+            l = str.Length - 1;
+            for (i = l; i >= 0; i--)
+            {
+                str1 = str1 + str[i];
+            }
+            Console.WriteLine("The string in Reverse  Order Is : {0}", str1);
+
+
+
+            return s;   
+        }
+    }
+
+
+
+
+
+
+
+
+}
---
+++ Session-04/Class2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_04
+{
+    internal class Class2
+    {
+
+        public Class2()
+        {
+
+        }
+
+
+
+        public int GetProduct(int i)
+        {
+            int n;
+            //Console.WriteLine("Please enter a number to get its factorial: ");
+            //num = Convert.ToInt32(Console.ReadLine());
+
+            n = i; 
+
+            while (i > 0)
+            {
+                for (int j = n - 1; j > 0; j--)
+                {
+                    n *= i;
+                }
+                //Console.WriteLine("Factorial of {0}! = {1}\n", num, n);
+                i--;
+            }
+            return i;
+
+          
+        }
+
+
+
+
+
+
+
+        public int GetSum(int i)
+        {
+            int sum = 0;
+            for (int x = 1; x <= i; x++)
+                sum += i;
+
+            return sum;
+        }
+    }
+
+    }
+
+
